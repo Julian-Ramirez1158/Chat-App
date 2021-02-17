15727c505db382a3b81f082497b3bd222cddbd5f
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2021-02-17 15:29:47 -0500

    Create App.js with Router and 2 paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import App from './App';
+import Chat from './components/Chat';
+import Join from './components/Join';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+}
+
+export default App;
