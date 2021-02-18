addc97c9756d4130a1a86d95ce7dd14d07e3217a
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2021-02-17 19:01:07 -0500

    Implement client functions into index.js with socket functions

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,44 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addClient, removeClient, getClient, getClientsInRoom} = require('./clients.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+
 
 io.on('connection', (socket) => {
-    console.log("we have a new connection");
+    socket.on('join', ({room, name}, callback) => {
+        const {error, client} = addClient({id: socket.id, name, room});
+
+        if(error) return callback(error);
+
+        socket.emit('message', {client: 'admin', text: `${client.name}, Welcome to the chat ${client.room}!`});
+        socket.broadcast.to(client.room).emit('message', {client: 'admin', text: `${client.name} has joined the chat!`});
+
+
+        socket.join(client.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const client = getClient(socket.id);
+
+        io.to(client.room).emit('message', {client: client.name, text: message});
+
+        callback();
+    });
     
     socket.on('disconnect', () => {
         console.log('user has left');
